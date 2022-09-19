18c2105c24042ee8571b7efffa12745142379e04
BayuTamala <bayuardo03@gmail.com>
2022-09-19 21:18:15 +0700

    Upload Pertama

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+  {
+  echo "<h1>Perkenalkan</h1>";
+  echo"Nama saya Bayu Tamala
+  Saya tingga di parit haji muksin 2
+  olah raga yang saya sukai adalah
+  Bulutangkis, futsal, dan voli";
+  }
+}
